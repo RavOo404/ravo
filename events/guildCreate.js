adb8c2fb3a1c26a633f1497551794e5d74087da2
Matheros <98669451+matheros0001@users.noreply.github.com>
2022-08-18 22:00:05 +0300

    Initial commit

---
+++ events/guildCreate.js
@@ -0,0 +1,39 @@
+const { Client, Guild, MessageEmbed } = require("discord.js")
+
+/**
+ * 
+ * @param {Client} client 
+ * @param {Guild} guild 
+ */
+
+
+module.exports = (client, guild) => {
+  if (guild.memberCount < 100) {
+	return guild.leave()
+       }
+
+ let channel = client.channels.cache.get("997071211282567178");
+ const joinEmbed = new MessageEmbed()
+       .setColor("RANDOM")
+       .setTitle("New Server!!")
+       .setThumbnail(guild.iconURL())
+       .addField(
+         "Guild:",
+         `i just joined to here **${guild.name}**.\n\nGuild Members: [**${guild.memberCount}**]`,
+         true
+       )
+       .addField(
+         "Owner:",
+         `${guild.owner}`,
+         true
+       )
+       .addField(
+         "Servers:",
+         `[${client.guilds.cache.size}]`,
+         true
+       )
+       .setTimestamp() // moment().format('LLL'),
+       .setFooter(`Bzhe la xom`);
+    channel.send(joinEmbed);
+
+}
